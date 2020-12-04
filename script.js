39af195ac95f35b6e5f48c12782827acc8376c05
padillal <padillal48@yahoo.com>
2020-12-04 15:32:42 -0800

    creating factory and modules for game

---
+++ script.js
@@ -0,0 +1,28 @@
+//creating a gameBoard using a module
+const gameBoard = (() => {
+  //board is generated as an array
+  const generateBoard = [];
+
+  //board is returned
+  return{generateBoard};
+})();
+
+//creating displayController using a module
+const displayController = (() => {
+  //creating turn for players
+  let turn;
+
+  return{turn};
+})();
+
+const Player = function(name, mark){
+  let name = name;
+  let mark = mark;
+  let score = 0;
+
+  const incScore = () => {
+    score += 1;
+  }
+
+  return {name, mark, incScore};
+}
