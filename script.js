e610271e35e0fbdc057c817d11a4f48290963fe8
padillal <padillal48@yahoo.com>
2020-12-13 22:59:12 -0800

    working on script

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
-//creating a gameBoard using a module
-// const gameBoard = (() => {
-  //board is generated as an array
-  // const generateBoard = [];
+// creating a gameBoard using a module
+const GameBoard = (() => {
+  // board is created as an array
+  let board = [];
+  // generate board
+  const generateBoard = () => {
+    for(let i = 0; i < 9; i++){
+      board[i] = "";
+    }
+  }
+  // mark cells within board
+  const markCell = (cellID, symbol) => {
+    board[cellID] = symbol;
+    return true;
+  }
 
-  //board is returned
-  // return{generateBoard};
-// })();
+  const resetBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      //reset here
+      }
+  }
 
-//creating displayController using a module
-// const displayController = (() => {
-  //creating turn for players
-  // let turn;
+  // board is returned
+  return{generateBoard, markCell};
+})();
 
-  // return{turn};
-// })();
+const GameLogic = ( () => {
+  // start game
+  const startGame = () => {
+    GameBoard.generateBoard();
+    DisplayController.attachEventListeners();
+  }
 
-// const Player = function(name, mark){
-  // let name = name;
-  // let mark = mark;
-  // let score = 0;
+  // Mark cell
+  const markCell = (i) => {
+  }
 
-  // const incScore = () => {
-    // score += 1;
-  // }
 
-  // return {name, mark, incScore};
-// }
+  return {startGame, markCell};
+} )()
+
+// creating displayController using a module
+const DisplayController = (() => {
+  // Mark the following cell with the given marker
+  const markCell = (cellNum, marker) => {
+    // $("#"+cellNum).text(marker);
+  }
+  const attachEventListeners = () => {
+    for(let i = 0; i < 9; i++ ){
+      $("#"+i).click(function() {
+        GameLogic.markCell(i);
+      });
+    }
+  }
+  return {attachEventListeners, markCell};
+})();
+
+const Player = function(name, mark){
+  const getName = () => {
+    return name;
+  }
+  const getMark = () => {
+    return mark;
+  }
+
+  let score = 0;
+  const incScore = () => {
+    score += 1;
+  }
+  return {name, mark, incScore};
+}
+
+const AttachEventlisteners = (() => {
+  $("#start").click(function() {
+    GameLogic.startGame();
+});
