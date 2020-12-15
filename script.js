ab3aee6dee4bd364bcf047cbe7a808367564e9e4
padillal <padillal48@yahoo.com>
2020-12-14 19:06:54 -0800

    restarting script

--- script.js
+++ script.js
@@ -1,76 +1,98 @@
-// creating a gameBoard using a module
-const GameBoard = (() => {
-  // board is created as an array
+
+// const moduleTemplate = (() => {
+//   return {};
+// })();
+//
+// const factoryTemplate = (param1, param2) => {
+//   const func = () => TODO;
+//   return {};
+// };
+const gameBoard = (() => {
   let board = [];
-  // generate board
+
   const generateBoard = () => {
     for(let i = 0; i < 9; i++){
       board[i] = "";
     }
   }
-  // mark cells within board
-  const markCell = (cellID, symbol) => {
-    board[cellID] = symbol;
-    return true;
-  }
-
-  const resetBoard = () => {
-    for (let i = 0; i < 9; i++) {
-      //reset here
-      }
-  }
-
-  // board is returned
-  return{generateBoard, markCell};
+  return {generateBoard};
 })();
 
-const GameLogic = ( () => {
-  // start game
-  const startGame = () => {
-    GameBoard.generateBoard();
-    DisplayController.attachEventListeners();
-  }
-
-  // Mark cell
-  const markCell = (i) => {
-  }
 
 
-  return {startGame, markCell};
-} )()
+// creating a gameBoard using a module
+// const GameBoard = (() => {
+//   // board is created as an array
+//   let board = [];
+//   // generate board
+//   const generateBoard = () => {
+//     for(let i = 0; i < 9; i++){
+//       board[i] = "";
+//     }
+//   }
+//   // mark cells within board
+//   const markCell = (cellID, symbol) => {
+//     board[cellID] = symbol;
+//     return true;
+//   }
+//
+//   const resetBoard = () => {
+//     for (let i = 0; i < 9; i++) {
+//       //reset here
+//       }
+//   }
+//
+//   // board is returned
+//   return{generateBoard, markCell};
+// })();
 
-// creating displayController using a module
-const DisplayController = (() => {
-  // Mark the following cell with the given marker
-  const markCell = (cellNum, marker) => {
-    // $("#"+cellNum).text(marker);
-  }
-  const attachEventListeners = () => {
-    for(let i = 0; i < 9; i++ ){
-      $("#"+i).click(function() {
-        GameLogic.markCell(i);
-      });
-    }
-  }
-  return {attachEventListeners, markCell};
-})();
-
-const Player = function(name, mark){
-  const getName = () => {
-    return name;
-  }
-  const getMark = () => {
-    return mark;
-  }
-
-  let score = 0;
-  const incScore = () => {
-    score += 1;
-  }
-  return {name, mark, incScore};
-}
-
-const AttachEventlisteners = (() => {
-  $("#start").click(function() {
-    GameLogic.startGame();
-});
+// const GameLogic = ( () => {
+//   // start game
+//   const startGame = () => {
+//     GameBoard.generateBoard();
+//     DisplayController.attachEventListeners();
+//   }
+//
+//   // Mark cell
+//   const markCell = (i) => {
+//   }
+//
+//
+//   return {startGame, markCell};
+// } )()
+//
+// // creating displayController using a module
+// const DisplayController = (() => {
+//   // Mark the following cell with the given marker
+//   const markCell = (cellNum, marker) => {
+//     // $("#"+cellNum).text(marker);
+//   }
+//   const attachEventListeners = () => {
+//     for(let i = 0; i < 9; i++ ){
+//       $("#"+i).click(function() {
+//         GameLogic.markCell(i);
+//       });
+//     }
+//   }
+//   return {attachEventListeners, markCell};
+// })();
+//
+// const Player = function(name, mark){
+//   const getName = () => {
+//     return name;
+//   }
+//   const getMark = () => {
+//     return mark;
+//   }
+//
+//   let score = 0;
+//   const incScore = () => {
+//     score += 1;
+//   }
+//   return {name, mark, incScore};
+// }
+//
+// const AttachEventlisteners = (() => {
+//   $("#start").click(function() {
+//     GameLogic.startGame();
+// })}
